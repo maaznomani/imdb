5260810867b481f2f9044f87f0312fbe229654b9
maaznomani <70318597+maaznomani@users.noreply.github.com>
2021-11-01 12:55:49 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,26 +9,6 @@ const App = () => {
       <Header />
       <Route path='/movies/:keyword' component={HomeScreen} exact/>
       <Route path='/' component={HomeScreen} exact/>
-      {/* <div className='App'>
-        
-        <main className='py-3'>
-          <Container>
-            
-            <Route path='/product/:id' component={ProductScreen}/>
-            <Route path='/cart/:id?' component={CartScreen}/>
-            <Route path='/login' component={LoginScreen}/>
-            <Route path='/register' component={RegisterScreen}/>
-            <Route path='/profile' component={ProfileScreen}/>
-            <Route path='/shipping' component={ShippingScreen}/>
-            <Route path='/payment' component={PaymentScreen}/>
-            <Route path='/placeorder' component={PlaceOrderScreen}/>
-            <Route path='/order/:id' component={OrderScreen}/>
-            <Route path='/admin/userList' component={AdminUsersScreen}/>
-            <Route path='/admin/user/:id/edit' component={UserEditScreen}/>
-          </Container>
-        </main>
-        <Footer />
-      </div> */}
     </Router>
   )
 }
